bfc51c082516d76b7a7fa0bb5005866949c3cea5
Lucy Suddenly <suddenlylucy@gmail.com>
2018-11-16 16:57:50 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,7 +1,7 @@
 def my_each(array)
-  counter = array.length - 1
+  counter = array.length 
   while counter > 0 
-    yield(array[counter])
+    yield(array[counter - 1])
     counter -= 1
   end 
 end
